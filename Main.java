6a58d9fd9b4d42d0f3bb0f5ce07bd8107686884b
unknown <akshathas1692000@gmail.com>
2025-12-05 14:48:41 +0530

    adding printing statements

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ package com.jspiders;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("program start");
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and me!");
@@ -12,6 +13,7 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+            System.out.println("program end");
         }
     }
 }
